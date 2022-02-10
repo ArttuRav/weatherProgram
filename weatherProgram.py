b8fcc2edee4d0042e45a308dadc1b0e2fd6c6e1f
ArttuRav <75360698+ArttuRav@users.noreply.github.com>
2022-02-10 21:49:49 +0200

    Update weatherProgram.py

--- weatherProgram.py
+++ weatherProgram.py
@@ -5,13 +5,13 @@
 import tkinter as tk
 import requests, json
 from tkinter import END, ttk
+from datetime import datetime
+from threading import Thread
 
 class WeatherProgram(tk.Tk):
 
     def __init__(self):
         super().__init__()
-
-        # weatherData = WeatherInfo(self)
 
         # Creating window
         self.title('Weather program')
@@ -19,28 +19,40 @@ class WeatherProgram(tk.Tk):
         self.geometry('700x600')
         self['bg'] = 'lightblue'
 
-        # Creating label
-        # self.searchText = ttk.Label(
-            # self,
-            # text='Search city: ',
-            # font = ('Digital-7', 40))
+        self.canvas1 = tk.Canvas(self, bg='white', height=400, width=300).place(x='10', y='100')
+
+        self.tempLabel = tk.Label(self, text='Temperature:', font=('lucida', 12), background='white').place(x='15', y='100')
+        self.dateLabel = tk.Label(self, text=self.getDate(), font=('lucida', 10), background='lightblue')
+        self.timeLabel = tk.Label(self, text=self.getTime(), font=('lucida', 10), background='lightblue')
+        self.dateLabel.place(x='15', y='5')
+        self.timeLabel.place(x='100', y='5')
+
+        self.newThreadTime = Thread(target=self.update).start()
 
         self.entryCity = ttk.Entry(
             self,
-            width='15',
+            width='24',
             font='40')
 
         self.searchButton = ttk.Button(
             self,
             text='Search',
-            command=lambda:[self.getCity(), WeatherInfo.printData(self)])
+            command=lambda:[self.getCity()])
+
+        # Creating label
+        self.temperature = ttk.Label(
+            self,
+            text=lambda:self.displayInfo('0'),
+            font = (40))
 
         self.entryCity.insert(0, 'Enter a city...') # Adding a default value to be displayed
         self.entryCity.bind('<Button-1>', self.clearEntryDefault) # Removing the default value when clicked
 
         # Placements
-        self.entryCity.place(x='120', y='50')
-        self.searchButton.place(x='270', y='50')
+        self.entryCity.place(x='15', y='40')
+        self.searchButton.place(x='240', y='40')
+
+        self.update()
 
 
     def clearEntryDefault(self, event):
@@ -51,12 +63,33 @@ class WeatherProgram(tk.Tk):
 
         return self.cityName
 
+    def displayInfo(self, index):
+        self.infoList = WeatherInfo.jsonToString(self)
+
+        return self.infoList[index]
+
+    def getDate(self):
+        now = datetime.now()
+
+        return now.strftime('%B %d')
+
+    def getTime(self):
+        now = datetime.now()
+
+        return now.strftime('%H:%M')
+
+    def update(self):
+        self.timeLabel.config(text=self.getTime())
+
+        self.after(1000, self.update)
+
 class WeatherInfo():
 
     def __init__(self):
         super().__init__()
 
-    def printData(self):
+    def jsonToString(self):
+
         # API information
         self.apiKey = '936b51d488d6f3918dcaee06b7c69a9f'
         self.baseURL = 'http://api.openweathermap.org/data/2.5/weather?q='
@@ -65,7 +98,19 @@ class WeatherInfo():
         self.response = requests.get(self.completeURL)
         self.data = self.response.json()
 
-        print(self.data)
+        self.main = self.data['main']
+        self.weather = self.data['weather']
+
+        self.temperature = self.main['temp']
+        self.feelsTemp = self.main['feels_like']
+        self.pressure = self.main['pressure']
+        self.humidity = self.main['humidity']
+        self.visibility = self.data['visibility']
+        self.description = self.weather[0]['description']
+
+        self.infoList = [self.temperature, self.feelsTemp, self.pressure, self.humidity, self.visibility, self.description]
+
+        return self.infoList
 
 
 if __name__ == '__main__':
