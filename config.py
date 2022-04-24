03b61c31272103cb61c8804661ab1ab72406f05e
ArttuRav <75360698+ArttuRav@users.noreply.github.com>
2022-04-24 18:42:00 +0300

    Create config.py
    API and AWS keys have been hidden.

---
+++ config.py
@@ -0,0 +1,9 @@
+# API key
+api_key_default = 'API_KEY'
+api_key_backup = 'API_KEY'
+
+# AWS keys
+aws_access_keyid = 'AWS_ACCESS_KEYID'
+aws_secret_access_key = 'AWS_SECRET_ACCESS_KEY'
+
+api_key_index = 0
