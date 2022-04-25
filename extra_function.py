b633305537c8c1c8ae99129686200c8a381892ff
ArttuRav <75360698+ArttuRav@users.noreply.github.com>
2022-04-25 12:13:21 +0300

    Version 1.0
    Removed old non-functional code and cleaned up existing code.

--- extra_function.py
+++ extra_function.py
@@ -3,8 +3,6 @@ from requests_ip_rotator import ApiGateway
 import requests
 
 def calls_exceeded(complete_url):
-    api_key = config.api_key_backup
-
     gateway_url = 'http://api.openweathermap.org'
     gateway = ApiGateway(gateway_url, regions=['eu-west-1', 'eu-west-2'], access_key_id=config.aws_access_keyid, access_key_secret=config.aws_secret_access_key)
     gateway.start()
